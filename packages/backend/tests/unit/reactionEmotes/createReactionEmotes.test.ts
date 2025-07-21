c36fc5141a5c8400c5a82367d9eee630cba6fb7e
Elliott DuCharme <ellduc4@gmail.com>
2025-07-21 17:19:19 -0500

    Add unit tests for ReactionEmotes

---
+++ packages/backend/tests/unit/reactionEmotes/createReactionEmotes.test.ts
@@ -0,0 +1,40 @@
+import { createReactionEmoteRepository } from "@adapters/repositories/ReactionEmoteRepository";
+import { expect } from "vitest";
+
+import { invalidEmotes, validEmotes } from "../../testData/reactionEmotes";
+import { createTestDb } from "../../utils/testUtils";
+
+describe("Create ReactionEmote, valid data", () => {
+    test.each(validEmotes)("%s, %s", async (name, discordId) => {
+        const db = await createTestDb();
+        const emotes = createReactionEmoteRepository(db);
+
+        await emotes.create({ name, discordId, karmaValue: 0 });
+        const found = await emotes.findByNameAndDiscordId(name, discordId);
+        expect(found).not.toBeNull();
+    });
+});
+
+describe("Create ReactionEmote, throws when emote exists", () => {
+    test.each(validEmotes)("%s %s", async (name, discordId) => {
+        const db = await createTestDb();
+        const emotes = createReactionEmoteRepository(db);
+
+        const data = { name, discordId, karmaValue: 0 };
+
+        await emotes.create(data);
+        const found = await emotes.findByNameAndDiscordId(name, discordId);
+        expect(found).not.toBeNull();
+
+        await expect(emotes.create(data)).rejects.toThrow();
+    });
+});
+
+describe("Create ReactionEmote, throws for invalid emotes", () => {
+    test.each(invalidEmotes)("%s %s", async (name, discordId) => {
+        const db = await createTestDb();
+        const emotes = createReactionEmoteRepository(db);
+
+        await expect(emotes.create({ name, discordId, karmaValue: 0 })).rejects.toThrow();
+    });
+});
