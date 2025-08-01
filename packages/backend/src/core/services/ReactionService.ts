69edf71f579b7e81d1459d798a42b647f921c9ce
Elliott DuCharme <ellduc4@gmail.com>
2025-08-01 10:38:45 -0500

    Add integration test for removing all reactions from message

--- packages/backend/src/core/services/ReactionService.ts
+++ packages/backend/src/core/services/ReactionService.ts
@@ -1,10 +1,11 @@
 import type { ReactionEmoteRepository } from "@core/repositories/ReactionEmoteRepository";
 import type { ReactionRepository } from "@core/repositories/ReactionRepository";
-import type { MessageReaction, PartialMessageReaction, PartialUser, User } from "discord.js";
+import type { Message, MessageReaction, OmitPartialGroupDMChannel, PartialMessage, PartialMessageReaction, PartialUser, ReadonlyCollection, User } from "discord.js";
 
 export type ReactionService = {
     readonly addReaction: (reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser) => Promise<void>;
     readonly removeReaction: (reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser) => Promise<void>;
+    readonly removeReactionsForMessage: (message: OmitPartialGroupDMChannel<Message<boolean> | PartialMessage>, reactions: ReadonlyCollection<string, MessageReaction>) => Promise<void>;
 };
 
 export type ReactionServiceDeps = {
@@ -58,6 +59,14 @@ export const createReactionService = ({ reactionRepository, emoteRepository }: R
 
                 const data = { giverId: user.id, receiverId: message.author.id, channelId: channel.id, messageId: message.id, emoteId: reactionEmote.id };
                 await reactionRepository.delete(data);
+            } catch (e: unknown) {
+                console.error("Error removing reaction from message", e);
+            }
+        },
+
+        removeReactionsForMessage: async (message): Promise<void> => {
+            try {
+                await reactionRepository.deleteReactionsForMessage(message.id);
             } catch (e: unknown) {
                 console.error("Error removing reaction from message", e);
             }
