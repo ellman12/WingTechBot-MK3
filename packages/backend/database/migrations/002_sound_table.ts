431266e577a8c8f40b6b2dbbb1f1a61ed5d6e35c
Elliott DuCharme <ellduc4@gmail.com>
2025-09-20 15:06:38 -0500

    Change varchars to text in 002_sound_table

--- packages/backend/database/migrations/002_sound_table.ts
+++ packages/backend/database/migrations/002_sound_table.ts
@@ -2,24 +2,21 @@ import { Kysely, sql } from "kysely";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function up(db: Kysely<any>): Promise<void> {
-    // Create soundtags table
     await db.schema
         .createTable("soundtags")
         .addColumn("id", "serial", col => col.notNull().primaryKey())
-        .addColumn("name", "varchar(255)", col => col.notNull().unique())
+        .addColumn("name", "text", col => col.notNull().unique())
         .addColumn("created_at", "timestamp", col => col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`))
         .execute();
 
-    // Create sounds table
     await db.schema
         .createTable("sounds")
         .addColumn("id", "serial", col => col.notNull().primaryKey())
-        .addColumn("name", "varchar(255)", col => col.notNull().unique())
-        .addColumn("path", "varchar(255)", col => col.notNull().unique())
+        .addColumn("name", "text", col => col.notNull().unique())
+        .addColumn("path", "text", col => col.notNull().unique())
         .addColumn("created_at", "timestamp", col => col.notNull().defaultTo(sql`CURRENT_TIMESTAMP`))
         .execute();
 
-    // Create soundTags table
     await db.schema
         .createTable("sound_soundtags")
         .addColumn("id", "serial", col => col.notNull().primaryKey())
@@ -30,13 +27,11 @@ export async function up(db: Kysely<any>): Promise<void> {
         .addForeignKeyConstraint("fk_sound_soundtags_tag", ["tag"], "soundtags", ["id"])
         .execute();
 
-    // Create indexes
     await db.schema.createIndex("idx_sounds_name").on("sounds").column("name").execute();
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function down(db: Kysely<any>): Promise<void> {
-    // Drop tables
     await db.schema.dropTable("sound_soundtags").execute();
     await db.schema.dropTable("sounds").execute();
     await db.schema.dropTable("soundtags").execute();
