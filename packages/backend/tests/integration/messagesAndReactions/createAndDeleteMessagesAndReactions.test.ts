06c7fb565a163eaa510322e75da431d45ac9a48e
Elliott DuCharme <ellduc4@gmail.com>
2025-09-29 12:18:11 -0500

    Fix some fugly formatting by Prettier

--- packages/backend/tests/integration/messagesAndReactions/createAndDeleteMessagesAndReactions.test.ts
+++ packages/backend/tests/integration/messagesAndReactions/createAndDeleteMessagesAndReactions.test.ts
@@ -5,72 +5,69 @@ import { createMessagesAndReactions, getTestingChannel, recreateDatabase, setUpI
 const timeout = 180 * 1000;
 const delay = 3000;
 
-describe(
-    "Messages and Reactions integration tests",
-    async () => {
-        beforeAll(async () => {
-            await sleep(delay);
+//prettier-ignore
+describe("Messages and Reactions integration tests", async () => {
+    beforeAll(async () => {
+        await sleep(delay);
 
-            const bot = getApp().discordBot;
-            const channel = await getTestingChannel(bot);
-            await channel.send("Starting Create/Delete Messages/Reactions tests");
-        });
+        const bot = getApp().discordBot;
+        const channel = await getTestingChannel(bot);
+        await channel.send("Starting Create/Delete Messages/Reactions tests");
+    });
 
-        beforeEach(async () => {
-            await sleep(delay);
-            await recreateDatabase();
-        });
+    beforeEach(async () => {
+        await sleep(delay);
+        await recreateDatabase();
+    });
 
-        it("sends messages, adds reactions, removes both, verifies DB entries", async () => {
-            const totalMessages = 3,
-                reactionsPerMessage = 4;
-            const { channel, emotes, testerChannel, db } = await setUpIntegrationTest();
+    it("sends messages, adds reactions, removes both, verifies DB entries", async () => {
+        const totalMessages = 3;
+        const reactionsPerMessage = 4;
+        const { channel, emotes, testerChannel, db } = await setUpIntegrationTest();
 
-            const messages = await createMessagesAndReactions(channel, testerChannel, totalMessages, reactionsPerMessage, emotes);
-            await sleep(delay);
-            await verifyTesterReactions(db, totalMessages * reactionsPerMessage);
+        const messages = await createMessagesAndReactions(channel, testerChannel, totalMessages, reactionsPerMessage, emotes);
+        await sleep(delay);
+        await verifyTesterReactions(db, totalMessages * reactionsPerMessage);
 
-            //Delete message, verify reactions gone
-            let message = messages[0]!;
-            await message.delete();
-            await sleep(delay);
-            let foundMessages = await db.selectFrom("messages").where("id", "=", message.id).selectAll().execute();
-            let foundReactions = await db.selectFrom("reactions").where("message_id", "=", message.id).selectAll().execute();
-            expect(foundMessages).toHaveLength(0);
-            expect(foundReactions).toHaveLength(0);
+        //Delete message, verify reactions gone
+        let message = messages[0]!;
+        await message.delete();
+        await sleep(delay);
+        let foundMessages = await db.selectFrom("messages").where("id", "=", message.id).selectAll().execute();
+        let foundReactions = await db.selectFrom("reactions").where("message_id", "=", message.id).selectAll().execute();
+        expect(foundMessages).toHaveLength(0);
+        expect(foundReactions).toHaveLength(0);
 
-            //Remove reactions for message, then delete message
-            message = messages[1]!;
-            await message.reactions.removeAll();
-            await sleep(delay);
-            foundReactions = await db.selectFrom("reactions").where("message_id", "=", message.id).selectAll().execute();
-            expect(foundReactions).toHaveLength(0);
+        //Remove reactions for message, then delete message
+        message = messages[1]!;
+        await message.reactions.removeAll();
+        await sleep(delay);
+        foundReactions = await db.selectFrom("reactions").where("message_id", "=", message.id).selectAll().execute();
+        expect(foundReactions).toHaveLength(0);
 
-            await message.delete();
-            await sleep(delay);
-            foundMessages = await db.selectFrom("messages").where("id", "=", message.id).selectAll().execute();
-            expect(foundMessages).toHaveLength(0);
+        await message.delete();
+        await sleep(delay);
+        foundMessages = await db.selectFrom("messages").where("id", "=", message.id).selectAll().execute();
+        expect(foundMessages).toHaveLength(0);
 
-            //Remove all reactions with specific emotes, then delete message
-            message = messages[2]!;
-            for (let i = 0; i < reactionsPerMessage; i++) {
-                const [name, discordId] = emotes[i]!;
-                const reaction = message.reactions.cache.find(r => r.emoji.name === name && r.emoji.id === discordId)!;
-                await reaction.remove();
-            }
+        //Remove all reactions with specific emotes, then delete message
+        message = messages[2]!;
+        for (let i = 0; i < reactionsPerMessage; i++) {
+            const [name, discordId] = emotes[i]!;
+            const reaction = message.reactions.cache.find(r => r.emoji.name === name && r.emoji.id === discordId)!;
+            await reaction.remove();
+        }
 
-            await sleep(delay);
-            foundReactions = await db.selectFrom("reactions").where("message_id", "=", message.id).selectAll().execute();
-            expect(foundReactions).toHaveLength(0);
+        await sleep(delay);
+        foundReactions = await db.selectFrom("reactions").where("message_id", "=", message.id).selectAll().execute();
+        expect(foundReactions).toHaveLength(0);
 
-            await message.delete();
-            await sleep(delay);
-            foundMessages = await db.selectFrom("messages").where("id", "=", message.id).selectAll().execute();
-            expect(foundMessages).toHaveLength(0);
+        await message.delete();
+        await sleep(delay);
+        foundMessages = await db.selectFrom("messages").where("id", "=", message.id).selectAll().execute();
+        expect(foundMessages).toHaveLength(0);
 
-            foundReactions = await db.selectFrom("reactions").selectAll().execute();
-            expect(foundReactions).toHaveLength(0);
-        });
-    },
-    timeout
-);
+        foundReactions = await db.selectFrom("reactions").selectAll().execute();
+        expect(foundReactions).toHaveLength(0);
+    }, timeout);
+});
