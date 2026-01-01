a22d131bccb5687bd9d285de7007e94eff67f8b2
Dan Truong <truongtdan@gmail.com>
2026-01-01 07:38:36 -0500

    Fix constraint

--- packages/backend/database/migrations/007_add_reactions_index.ts
+++ packages/backend/database/migrations/007_add_reactions_index.ts
@@ -2,7 +2,7 @@ import { Kysely } from "kysely";
 
 //eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function up(db: Kysely<any>): Promise<void> {
-    await db.schema.createIndex("idx_reactions_columns").on("reactions").columns(["giver_id", "receiver_id", "channel_id", "message_id", "emote_id"]).execute();
+    await db.schema.createIndex("idx_reactions_columns").on("reactions").columns(["giver_id", "receiver_id", "channel_id", "message_id", "emote_id"]).unique().execute();
 }
 
 //eslint-disable-next-line @typescript-eslint/no-explicit-any
