dae5a6ea6285d3f4a5bbaad7651d27256293d412
Elliott DuCharme <ellduc4@gmail.com>
2026-07-30 10:04:17 -0500

    VoiceEventSounds now have short delays before they play

--- packages/backend/src/core/services/VoiceEventSoundsService.ts
+++ packages/backend/src/core/services/VoiceEventSoundsService.ts
@@ -2,6 +2,7 @@ import type { VoiceEventSoundsRepository } from "@adapters/repositories/VoiceEve
 import type { Config } from "@core/config/Config.js";
 import type { VoiceService } from "@core/services/VoiceService.js";
 import { randomArrayItem } from "@core/utils/probabilityUtils.js";
+import { sleep } from "@core/utils/timeUtils.js";
 import type { VoiceEventSoundType } from "@db/types.js";
 import { VoiceState } from "discord.js";
 
@@ -13,6 +14,12 @@ export type VoiceEventSoundsServiceDeps = {
     readonly config: Config;
     readonly voiceEventSoundsRepository: VoiceEventSoundsRepository;
     readonly voiceService: VoiceService;
+};
+
+//Delays in ms for each event type before the sound is played.
+const soundDelays = {
+    UserJoin: 250, //Ensures the person who joined can also hear their sound. Gives time for their connection to init.
+    UserLeave: 45, //Ensures others can hear this sound after the "leave call" Discord sound plays.
 };
 
 export const createVoiceEventSoundsService = ({ config, voiceEventSoundsRepository, voiceService }: VoiceEventSoundsServiceDeps): VoiceEventSoundsService => {
@@ -36,6 +43,11 @@ export const createVoiceEventSoundsService = ({ config, voiceEventSoundsReposito
 
         if (!voiceService.isConnected(guild.id)) {
             await voiceService.connect(guild, config.discord.defaultVoiceChannelId);
+        }
+
+        const delay = soundDelays[type];
+        if (delay) {
+            await sleep(delay);
         }
 
         await voiceService.playAudio(guild.id, sound.soundName!, botId, "VoiceEvent");
